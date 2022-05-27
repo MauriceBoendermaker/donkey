a61d5f0c47481f10cd994987441c52e73b977f5e
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-05-27 21:55:36 +0200

    welcome pagina -> index.php bij gezet

--- include/nav_boekingen.php
+++ include/nav_boekingen.php
@@ -3,7 +3,7 @@
 	<div class="crud-container">
 		<h2>Donkey Travel Administrative Tools</h2>
 		<ul class="nav nav-tabs">
-			<li class="nav-item"><a class="nav-link" aria-current="page" href="">Welcome</a></li>
+			<li class="nav-item"><a class="nav-link" aria-current="page" href="index.php">Welcome</a></li>
 			<li class="nav-item"><a class="nav-link active" href="boekingen.php">Boekingen</a></li>
 			<li class="nav-item"><a class="nav-link" href="gasten.php">Beheer</a></li>
 			<li class="nav-item ms-auto"><a class="nav-link text-danger" href="logout.php">Logout</a></li>
