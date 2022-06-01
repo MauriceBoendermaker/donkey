0959739997af165bc8054eb61595426716a251f2
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-06-01 12:12:14 +0200

    boekingen

---
+++ include/nav_klant.php
@@ -0,0 +1,9 @@
+<?php include "../include/head_klant.php"; ?>
+<div class="container">
+	<div class="crud-container">
+		<h2>Klanten pagina</h2>
+		<ul class="nav nav-tabs">
+			<li class="nav-item"><a class="nav-link active" href="boeking.php">Boekingen</a></li>
+			<li class="nav-item ms-auto"><a class="nav-link text-danger" href="../logout.php">Logout</a></li>
+		</ul>
+		<div class="crud-form row mx-0">
