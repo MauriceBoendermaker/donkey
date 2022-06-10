a1c239cb5499c0ec81634415f36a01a9949b1ab0
Jor02 <jorik.jellema@gmail.com>
2022-06-10 08:54:42 +0200

    Added update to herbergen

--- herbergen.php
+++ herbergen.php
@@ -1,5 +1,5 @@
 <?php include "include/nav_beheer.php"; ?>
-	<!-- debug print database Herbergen -->
+<!-- debug print database Herbergen -->
 <?php
 $db = new database\Database("localhost", "root", "", "donkey", null);
 $herbergen = $db->getHerbergen();
@@ -26,13 +26,18 @@ if (isset($_POST['cancel'])) {
 }
 
 if (isset($_POST['delete']) && isset($_POST['id'])) {
-    $db->deleteHerberg($_POST['id']);
-    home();
+	$db->deleteHerberg($_POST['id']);
+	home();
 }
 
 if (isset($_POST['add'])) {
-    $db->setHerberg(null, $_POST['naam'], $_POST['adres'], $_POST['email'], $_POST['telefoon'], $_POST['coordinaten']);
-    home();
+	$db->setHerberg(null, $_POST['naam'], $_POST['adres'], $_POST['email'], $_POST['telefoon'], $_POST['coordinaten']);
+	home();
+}
+
+if (isset($_POST['save'])) {
+	$db->setHerberg($_POST['id'], $_POST['naam'], $_POST['adres'], $_POST['email'], $_POST['telefoon'], $_POST['coordinaten']);
+	home();
 }
 
 function home()
@@ -44,38 +49,39 @@ function home()
 switch ($view) {
 	case 'edit':
 		$herberg = $db->getHerbergByID($id);
-		?>
+?>
 		<h3>Herberg gegevens wijzigen</h3>
 		<form action="" method="post">
+			<input type="hidden" name="id" id="id" value="<?php echo $herberg->getID(); ?>">
 			<div class="form-group">
 				<label for="naam">Naam:</label>
-				<input type='text' class='form-control' id='naam' value='<?php echo $herberg->getNaam(); ?>'>
+				<input type='text' class='form-control' id='naam' name='naam' value='<?php echo $herberg->getNaam(); ?>'>
 			</div>
 			<div class="form-group">
 				<label for="adres">Adres:</label>
-				<input type='text' class='form-control' id='adres' value='<?php echo $herberg->getAdres(); ?>'>
+				<input type='text' class='form-control' id='adres' name='adres' value='<?php echo $herberg->getAdres(); ?>'>
 			</div>
 			<div class="form-group">
 				<label for="email">Emailadres:</label>
-				<input type='email' class='form-control' id='email' value='<?php echo $herberg->getEmail(); ?>'>
+				<input type='email' class='form-control' id='email' name='email' value='<?php echo $herberg->getEmail(); ?>'>
 			</div>
 			<div class="form-group">
 				<label for="telefoon">Mobiele telefoonnummer:</label>
-				<input type='text' class='form-control' id='telefoon' value='<?php echo $herberg->getTelefoon(); ?>'>
+				<input type='text' class='form-control' id='telefoon' name='telefoon' value='<?php echo $herberg->getTelefoon(); ?>'>
 			</div>
 			<div class="form-group">
 				<label for="coordinaten">Coördinaten:</label>
-				<input type='text' class='form-control' id='coordinaten' value='<?php echo $herberg->getCoordinaten(); ?>'>
+				<input type='text' class='form-control' id='coordinaten' name='coordinaten' value='<?php echo $herberg->getCoordinaten(); ?>'>
 			</div>
-			<br/>
+			<br />
 			<button type="submit" name="save" class="btn btn-success">Bewaren</button>
 			<button type="submit" name="cancel" class="btn btn-primary">Annuleren</button>
 		</form>
-		<?php
-			break;
-		case 'delete':
-		    $herberg = $db->getHerbergByID($id);
-		?>
+	<?php
+		break;
+	case 'delete':
+		$herberg = $db->getHerbergByID($id);
+	?>
 		<h3>Herberg verwijderen</h3>
 		<form action="" method="post">
 			<input type="hidden" name="id" value="<?php echo $id; ?>">
@@ -99,89 +105,89 @@ switch ($view) {
 				<label for="coordinaten">Coördinaten:</label>
 				<input type='text' class='form-control' id='coordinaten' value='<?php echo $herberg->getCoordinaten(); ?>' disabled>
 			</div>
-			<br/>
+			<br />
 			<button name="delete" type="submit" class="btn btn-danger">Verwijderen</button>
-			<button name="cancel" type="submit"  class="btn btn-primary">Annuleren</button>
+			<button name="cancel" type="submit" class="btn btn-primary">Annuleren</button>
 		</form>
-		<?php
-			break;
-		case 'add':
-        ?>
-        <h3>Herberg toevoegen</h3>
-        <form action="" method="post">
-            <div class="form-group">
-                <label for="naam">Naam:</label>
-                <input type='text' class='form-control' id='naam' name='naam'>
-            </div>
-            <div class="form-group">
-                <label for="adres">Adres:</label>
-                <input type='text' class='form-control' id='adres' name='adres'>
-            </div>
-            <div class="form-group">
-                <label for="email">Emailadres:</label>
-                <input type='email' class='form-control' id='email' name='email'>
-            </div>
-            <div class="form-group">
-                <label for="telefoon">Mobiele telefoonnummer:</label>
-                <input type='text' class='form-control' id='telefoon' name='telefoon'>
-            </div>
-            <div class="form-group">
-                <label for="coordinaten">Coördinaten:</label>
-                <input type='text' class='form-control' id='coordinaten' name='coordinaten'>
-            </div>
-            <br/>
-            <button type="submit" name="add" class="btn btn-success">Toevoegen</button>
-            <button type="submit" name="cancel" class="btn btn-primary">Annuleren</button>
-        </form>
-		<?php
-			break;
-		default:
-?>
-<h3>Database Herbergen</h3>
-<table>
-	<tr>
-		<th>Naam</th>
-		<th>Adres</th>
-		<th>Email</th>
-		<th>Telefoon</th>
-		<th>Coördinaten</th>
-		<th class="d-flex justify-content-center"><a class='mx-1' href='?view=add'><button><i class="fa-solid fa-plus"></i></button></a></th>
-	</tr>
-<?php
-	foreach ($herbergen as $herberg) {
+	<?php
+		break;
+	case 'add':
+	?>
+		<h3>Herberg toevoegen</h3>
+		<form action="" method="post">
+			<div class="form-group">
+				<label for="naam">Naam:</label>
+				<input type='text' class='form-control' id='naam' name='naam'>
+			</div>
+			<div class="form-group">
+				<label for="adres">Adres:</label>
+				<input type='text' class='form-control' id='adres' name='adres'>
+			</div>
+			<div class="form-group">
+				<label for="email">Emailadres:</label>
+				<input type='email' class='form-control' id='email' name='email'>
+			</div>
+			<div class="form-group">
+				<label for="telefoon">Mobiele telefoonnummer:</label>
+				<input type='text' class='form-control' id='telefoon' name='telefoon'>
+			</div>
+			<div class="form-group">
+				<label for="coordinaten">Coördinaten:</label>
+				<input type='text' class='form-control' id='coordinaten' name='coordinaten'>
+			</div>
+			<br />
+			<button type="submit" name="add" class="btn btn-success">Toevoegen</button>
+			<button type="submit" name="cancel" class="btn btn-primary">Annuleren</button>
+		</form>
+	<?php
+		break;
+	default:
+	?>
+		<h3>Database Herbergen</h3>
+		<table>
+			<tr>
+				<th>Naam</th>
+				<th>Adres</th>
+				<th>Email</th>
+				<th>Telefoon</th>
+				<th>Coördinaten</th>
+				<th class="d-flex justify-content-center"><a class='mx-1' href='?view=add'><button><i class="fa-solid fa-plus"></i></button></a></th>
+			</tr>
+	<?php
+		foreach ($herbergen as $herberg) {
 
-		$coordinates = $herberg->getCoordinaten();
+			$coordinates = $herberg->getCoordinaten();
 
-		$coordinatesWithout = preg_replace("/[^0-9. ]/", "", $coordinates);
+			$coordinatesWithout = preg_replace("/[^0-9. ]/", "", $coordinates);
 
-		$trimOutput = $coordinatesWithout;
-		$arr = explode(' ',trim($trimOutput));
-		if ($coordinatesWithout != "") {
-			$coord1 = $arr[0];
-		} else {
-			$coord1 = "";
-		}
-		if ($coordinatesWithout != "") {
-			$coord2 = $arr[1];
-		} else {
-			$coord2 = "";
-		}
+			$trimOutput = $coordinatesWithout;
+			$arr = explode(' ', trim($trimOutput));
+			if ($coordinatesWithout != "") {
+				$coord1 = $arr[0];
+			} else {
+				$coord1 = "";
+			}
+			if ($coordinatesWithout != "") {
+				$coord2 = $arr[1];
+			} else {
+				$coord2 = "";
+			}
 
-		$output = "https://graphhopper.com/maps/?point=" . $coord1 . " " . $coord2 . "&point=" . $coord1 . " " . $coord2 . "&locale=en-US&elevation=false&profile=car&use_miles=false&layer=OpenStreetMap.de";
+			$output = "https://graphhopper.com/maps/?point=" . $coord1 . " " . $coord2 . "&point=" . $coord1 . " " . $coord2 . "&locale=en-US&elevation=false&profile=car&use_miles=false&layer=OpenStreetMap.de";
 
-		echo "<tr>";
-		echo "<td>" . $herberg->getNaam() . "</td>";
-		echo "<td>" . $herberg->getAdres() . "</td>";
-		echo "<td>" . $herberg->getEmail() . "</td>";
-		echo "<td>" . $herberg->getTelefoon() . "</td>";
-		echo "<td><a target='_blank' href='" . $output . "'>" . $herberg->getCoordinaten() ."</td>";
-		echo "<td class='px-0 d-flex justify-content-center'>
+			echo "<tr>";
+			echo "<td>" . $herberg->getNaam() . "</td>";
+			echo "<td>" . $herberg->getAdres() . "</td>";
+			echo "<td>" . $herberg->getEmail() . "</td>";
+			echo "<td>" . $herberg->getTelefoon() . "</td>";
+			echo "<td><a target='_blank' href='" . $output . "'>" . $herberg->getCoordinaten() . "</td>";
+			echo "<td class='px-0 d-flex justify-content-center'>
 				<a class='mx-1' href='?id={$herberg->getID()}&view=edit'><button><i class='fa-solid fa-pen-to-square'></i></button></a>
 				<a class='mx-1' href='?id={$herberg->getID()}&view=delete'><button><i class='fa-solid fa-trash-can'></i></button></a>
 			</td>";
-		echo "</tr>";
-	}
-	echo "</table>";
+			echo "</tr>";
+		}
+		echo "</table>";
 }
-?>
-<?php include "include/footer.php"; ?>
+	?>
+	<?php include "include/footer.php"; ?>
