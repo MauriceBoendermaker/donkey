5e7860453a2382e8a0aeea5729d8bc0a12e28643
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-06-14 08:59:48 +0200

    changed if() else to Switch statement

--- js/script.js
+++ js/script.js
@@ -1,21 +1,23 @@
-    var sPath = window.location.pathname;
-    var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
+var sPath = window.location.pathname;
+var sPage = sPath.substring(sPath.lastIndexOf('/') + 1);
 
-    if(sPage === "index.php") {
-    document.getElementById("nav-index").classList.add('active');
-}
-    else if(sPage === "gasten.php") {
-    document.getElementById("nav-gasten").classList.add('active');
-}
-    else if(sPage === "herbergen.php") {
-    document.getElementById("nav-herbergen").classList.add('active');
-}
-    else if(sPage === "restaurants.php") {
-    document.getElementById("nav-restaurants").classList.add('active');
-}
-    else if(sPage === "tochten.php") {
-    document.getElementById("nav-tochten").classList.add('active');
-}
-    else if(sPage === "status.php") {
-    document.getElementById("nav-statussen").classList.add('active');
+switch(sPage) {
+    case "index.php":
+        document.getElementById("nav-index").classList.add('active');
+        break;
+    case "gasten.php":
+        document.getElementById("nav-gasten").classList.add('active');
+        break;
+    case "herbergen.php":
+        document.getElementById("nav-herbergen").classList.add('active');
+        break;
+    case "restaurants.php":
+        document.getElementById("nav-restaurants").classList.add('active');
+        break;
+    case "tochten.php":
+        document.getElementById("nav-tochten").classList.add('active');
+        break;
+    case "status.php":
+        document.getElementById("nav-statussen").classList.add('active');
+        break;
 }
