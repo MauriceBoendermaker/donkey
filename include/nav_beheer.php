462a52ec696662855370b958dc843200f141f3b5
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-06-15 11:42:17 +0200

    formatting

--- include/nav_beheer.php
+++ include/nav_beheer.php
@@ -3,10 +3,21 @@
 		<div class="crud-container">
 			<h2>Donkey Travel Administrative Tools</h2>
 			<ul class="nav nav-tabs">
-				<li class="nav-item"><a class="nav-link" aria-current="page" href="index.php">Welcome</a></li>
-				<li class="nav-item"><a class="nav-link" href="boekingen.php">Boekingen</a></li>
-				<li class="nav-item"><a class="nav-link active" href="gasten.php">Beheer</a></li>
-				<li class="nav-item ms-auto"><a class="nav-link text-danger" href="logout.php">Logout</a></li>
+				<li class="nav-item">
+					<a class="nav-link" aria-current="page" href="index.php">Welcome</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="boekingen.php">Boekingen</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link active" href="gasten.php">Beheer</a>
+				</li>
+				<li class="nav-ite ms-auto">
+					<a id="nav-klant-account" class="nav-link" href="klant/account.php">Account</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link text-danger" href="logout.php">Logout</a>
+				</li>
 			</ul>
 			<div class="crud-form row mx-0 ps-0">
 				<div class="col-md-auto">
--- include/nav_klant.php
+++ include/nav_klant.php
@@ -1,9 +1,16 @@
 <?php include "../include/head_klant.php"; ?>
 <div class="container">
 	<div class="crud-container">
-		<h2>Klanten pagina</h2>
+		<h2>Mijn Donkey Travel</h2>
 		<ul class="nav nav-tabs">
-			<li class="nav-item"><a class="nav-link active" href="boeking.php">Boekingen</a></li>
-			<li class="nav-item ms-auto"><a class="nav-link text-danger" href="../logout.php">Logout</a></li>
+			<li class="nav-item">
+				<a id="nav-klant-beheer" class="nav-link" href="boeking.php">Boekingen</a>
+			</li>
+			<li class="nav-item">
+				<a id="nav-klant-account" class="nav-link" href="account.php">Account</a>
+			</li>
+			<li class="nav-item ms-auto">
+				<a class="nav-link text-danger" href="../logout.php">Logout</a>
+			</li>
 		</ul>
 		<div class="crud-form row mx-0">
