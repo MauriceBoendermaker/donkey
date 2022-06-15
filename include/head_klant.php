30ad654d0856ca4bb365193990994ea75ec4c1d0
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-06-15 11:41:06 +0200

    session klant

--- include/head_klant.php
+++ include/head_klant.php
@@ -2,6 +2,27 @@
 //using the database/Database.php class
 include_once '../database/Database.php';
 
+session_start();
+
+function endsWith($string, $endString)
+{
+	$len = strlen($endString);
+	if ($len == 0) {
+		return true;
+	}
+	return (substr($string, -$len) === $endString);
+}
+
+// check if user is on login page
+if (!endsWith($_SERVER['PHP_SELF'], 'login.php') && !endsWith($_SERVER['PHP_SELF'], 'register.php')) {
+	// check if the user is logged in
+	if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == false) {
+		// redirect to index.php
+		header('Location: ../login.php');
+		exit;
+	}
+}
+
 ?>
 <!-- crud+s main page styled by bootstrap -->
 <!DOCTYPE html>
