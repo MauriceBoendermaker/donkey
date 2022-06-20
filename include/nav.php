3f7aa60e0adb754a628fe322a7506f55b63f162f
Maurice Boendermaker <mauriceboendermaker@gmail.com>
2022-06-20 17:10:56 +0200

    Set active class for current page

--- include/nav.php
+++ include/nav.php
@@ -2,15 +2,21 @@
 <div class="container">
 	<div class="crud-container">
 		<h2>Donkey Travel Administrative Tools</h2>
+		<?php
+		$welcome = endsWith($_SERVER['REQUEST_URI'], "/");
+
+		$boekingen = endsWith($_SERVER['REQUEST_URI'], "boekingen");
+
+		?>
 		<ul class="nav nav-tabs">
 			<li class="nav-item">
-				<a class="nav-link <?php if (endsWith($_SERVER['REQUEST_URI'], "/")) echo "active\" aria-current=\"page"; ?>" aria-current="page" href="./">Welcome</a>
+				<a class="nav-link <?php if ($welcome) echo "active\" aria-current=\"page"; ?>" aria-current="page" href="./">Welcome</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link <?php if (endsWith($_SERVER['REQUEST_URI'], "boekingen")) echo "active\" aria-current=\"page"; ?>" href="boekingen">Boekingen</a>
+				<a class="nav-link <?php if ($boekingen) echo "active\" aria-current=\"page"; ?>" href="boekingen">Boekingen</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link <?php if (endsWith($_SERVER['REQUEST_URI'], "gasten")) echo "active\" aria-current=\"page"; ?>" href="gasten">Beheer</a>
+				<a class="nav-link <?php if (!$welcome && !$boekingen) echo "active\" aria-current=\"page"; ?>" href="gasten">Beheer</a>
 			</li>
 			<li class="nav-item ms-auto">
 				<a class="nav-link text-danger" href="logout">Logout</a>
