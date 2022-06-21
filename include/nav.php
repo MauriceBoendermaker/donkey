122a5b07903910344643b2f80a9170484917dfeb
Jor02 <jorik.jellema@gmail.com>
2022-06-21 11:32:16 +0200

    Added logged in user info

--- include/nav.php
+++ include/nav.php
@@ -1,7 +1,23 @@
 <?php include "include/head.php"; ?>
+<?php
+	function formatPhone($phone)
+	{
+		$phone = preg_replace("/[^0-9]/", "", $phone);
+		$phone = preg_replace('~.*(\d{3})[^\d]{0,7}(\d{3})[^\d]{0,7}(\d{4}).*~', "$1-$2-$3", $phone);
+		return $phone;
+	}
+?>
 <div class="container">
 	<div class="crud-container">
-		<h2>Donkey Travel Administrative Tools</h2>
+		<div class="row">
+			<h2 class="col-5">Donkey Travel Administrative Tools</h2>
+			<div class="col-7">
+				<div class="float-end">
+					<p class="my-0">Ingelogd als:</p>
+					<p class="my-0 text-end fw-light"><?php echo $_SESSION['naam'] . " [ " . $_SESSION['email'] . " ]" . " ( + " . formatPhone($_SESSION['telefoon']) . " )"; ?></p>
+				</div>
+			</div>
+		</div>
 		<?php
 		$welcome = endsWith($_SERVER['REQUEST_URI'], "/");
 
--- include/nav_klant.php
+++ include/nav_klant.php
@@ -1,7 +1,22 @@
 <?php include "./include/head_klant.php"; ?>
+<?php
+	function formatPhone($phone) {
+		$phone = preg_replace("/[^0-9]/", "", $phone);
+		$phone = preg_replace('~.*(\d{3})[^\d]{0,7}(\d{3})[^\d]{0,7}(\d{4}).*~', "$1-$2-$3", $phone);
+		return $phone;
+	}
+?>
 <div class="container">
 	<div class="crud-container">
-		<h2>Mijn Donkey Travel</h2>
+		<div class="row">
+			<h2 class="col-4">Mijn Donkey Travel</h2>
+			<div class="col-8">
+				<div class="float-end">
+					<p class="my-0">Ingelogd als:</p>
+					<p class="my-0 text-end fw-light"><?php echo $_SESSION['naam'] . " [ " . $_SESSION['email'] . " ]" . " ( + " . formatPhone($_SESSION['telefoon']) . " )"; ?></p>
+				</div>
+			</div>
+		</div>
 		<ul class="nav nav-tabs">
 			<li class="nav-item">
 				<a id="nav-klant-beheer" class="nav-link <?php if (endsWith($_SERVER['REQUEST_URI'], "boekingen")) echo "active\" aria-current=\"page"; ?>" href="boekingen">Boekingen</a>
