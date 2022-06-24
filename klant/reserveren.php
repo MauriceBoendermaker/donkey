7899f74244f180394d8af662363549b1bb7ecfdd
Jor02 <jorik.jellema@gmail.com>
2022-06-24 14:42:12 +0200

    Added map to reservation screen

--- klant/reserveren.php
+++ klant/reserveren.php
@@ -21,32 +21,58 @@ if (isset($_POST['submit'])) {
 }
 ?>
 <h3>Boeking Reserveren</h3>
-<form action="reserveren" method="post">
-    <?php if (isset($error)) {
-        echo '
+<div class="row">
+    <form class="col-md-7 h-100 position-relative" action="reserveren" method="post">
+        <?php if (isset($error)) {
+            echo '
 					<div class="alert alert-danger mt-4" role="alert">
 						<i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
 						<span class="sr-only">Error:</span>
 						Datum moet nieuwer zijn dan ' . date("d-m-Y") .
-            '</div>
+                '</div>
 				';
-    } ?>
-    <div class="form-group mt-2">
-        <label for="startdatum">Startdatum:</label>
-        <input type="date" class="form-control" id="startdatum" name="startdatum" placeholder="Startdatum">
+        } ?>
+        <div class="form-group mt-2">
+            <label for="startdatum">Startdatum:</label>
+            <input type="date" class="form-control" id="startdatum" name="startdatum" placeholder="Startdatum">
+        </div>
+        <div class="form-group mt-2">
+            <label for="tocht">Tocht:</label>
+            <select class="form-select" aria-label="Select tocht" id="tocht" name="tochtID">
+                <?php foreach ($db->getTochten() as $tocht) { ?>
+                    <option value="<?php echo $tocht->getID(); ?>">
+                        <?php echo $tocht->getOmschrijving() . " (" . $tocht->getAantalDagen() . " dagen)"; ?>
+                    <?php } ?>
+            </select>
+        </div>
+        <!-- submit button -->
+        <div class="position-absolute bottom-0 right-0 pb-2">
+            <button type="submit" name="submit" class="btn btn-primary mt-3">Boeking reserveren</button>
+            <a href="boekingen"><button type="button" class="btn btn-primary mt-3">Terug</button></a>
+        </div>
+    </form>
+    <div class="col-md-5">
+        <iframe id="view" class="map pb-2" src="../view?RouteName=Altlay"></iframe>
     </div>
-    <div class="form-group mt-2">
-        <label for="tocht">Tocht:</label>
-        <select class="form-select" aria-label="Select tocht" name="tochtID">
-            <?php foreach ($db->getTochten() as $tocht) { ?>
-                <option value="
-                    <?php echo $tocht->getID(); ?>">
-                    <?php echo $tocht->getOmschrijving() . " (" . $tocht->getAantalDagen() . " dagen)"; ?>
-                <?php } ?>
-        </select>
-    </div>
-    <!-- submit button -->
-    <button type="submit" name="submit" class="btn btn-primary mt-3">Boeking reserveren</button>
-    <a href="boekingen"><button type="button" class="btn btn-primary mt-3">Terug</button></a>
-</form>
+    <script>
+        $("#tocht").on('change', function() {
+            var select = $("#tocht");
+            var file = getFile(parseInt(select.val()));
+            $("#view").attr('src', "../view?RouteName=" + file);
+        });
+
+        function getFile(id) {
+            switch (id) {
+                <?php
+                foreach ($db->getTochten() as $tocht) {
+                    echo "case " . $tocht->getID() . ":\n";
+                    echo "return \"" . $tocht->getRoute() . "\";\n";
+                }
+                ?>
+                default:
+                    return "";
+            }
+        }
+    </script>
+</div>
 <?php include "./include/footer.php"; ?>
